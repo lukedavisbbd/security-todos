e8678d969969c900129aa9f1cad546d635355402
Luke Davis <luke.davis@sanlam.co.za>
2025-06-04 23:15:09 +0200

    Polish Auth Modals

--- frontend/src/lib/modals/RegisterModal.svelte
+++ frontend/src/lib/modals/RegisterModal.svelte
@@ -3,15 +3,26 @@
     import { fade } from "svelte/transition";
     import qrcode from 'qrcode';
     import Spinner from "../Spinner.svelte";
-    import { RegisterRequestSchema } from "common";
-    import { register } from "../../util/auth";
+    import { LoginRequestSchema, RegisterRequestSchema } from "common";
+    import { login, register } from "../../util/auth";
     import { z } from "zod/v4";
+
+    /**
+     * @param {MouseEvent | KeyboardEvent} e
+     */
+    const tryClose = (e) => {
+        if (e instanceof KeyboardEvent && e.key != 'Escape') {
+            return;
+        }
+        if (!registrationPromise) {
+            close();
+        }
+    };
 
     const performRegisteration = async () => {
         registrationErrors = null;
         
         if (password.trim() != passwordConfirm.trim()) {
-            registrationPromise = null;
             registrationErrors = {
                 errors: [],
                 properties: {
@@ -30,7 +41,6 @@
         });
 
         if (request.error) {
-            registrationPromise = null;
             registrationErrors = z.treeifyError(request.error);
             return;
         }
@@ -38,25 +48,56 @@
         const result = await register(request.data);
 
         if (!result) {
-            registrationPromise = null;
             registrationErrors = {
                 errors: ['Failed to register.'],
             };
             return;
         }
         
-        if ('ok' in result) {
-            return await qrcode.toDataURL(result.ok.totpUri, {
-                color: {
-                    dark: '#000',
-                    light: '#fff0',
-                },
-                margin: 0,
-                width: 512,
-            });
-        } else {
-            registrationPromise = null;
+        if ('err' in result) {
             registrationErrors = /** @type {import('zod/v4/core').$ZodErrorTree<import('common').RegisterRequest>} */(
+                result.err.data
+            );
+            return;
+        }
+            
+        return await qrcode.toDataURL(result.ok, {
+            color: {
+                dark: '#000',
+                light: '#fff0',
+            },
+            margin: 0,
+            width: 512,
+        });
+    };
+
+    const performLogin = async () => {
+        loginErrors = null;
+        
+        const request = LoginRequestSchema.safeParse({
+            email,
+            password,
+            twoFactor,
+        });
+
+        if (request.error) {
+            loginErrors = z.treeifyError(request.error);
+            return;
+        }
+
+        const result = await login(request.data);
+
+        if (!result) {
+            loginErrors = {
+                errors: ['Failed to sign in.'],
+            };
+            return;
+        }
+        
+        if ('ok' in result) {
+            close();
+        } else {
+            loginErrors = /** @type {import('zod/v4/core').$ZodErrorTree<import('common').LoginRequest>} */(
                 result.err.data
             );
         }
@@ -69,15 +110,17 @@
     let password = $state("");
     let passwordConfirm = $state("");
     let name = $state("");
+    let twoFactor = $state("");
     
     /** @type {Promise<string | undefined> | null} */
     let registrationPromise = $state(null);
     /** @type {import('zod/v4/core').$ZodErrorTree<import('common').RegisterRequest> | null} */
     let registrationErrors = $state(null);
-
-    /** @type {HTMLFormElement} */
-    // svelte-ignore non_reactive_update
-    let form;
+    
+    /** @type {Promise<void> | null} */
+    let loginPromise = $state(null);
+    /** @type {import('zod/v4/core').$ZodErrorTree<import('common').LoginRequest> | null} */
+    let loginErrors = $state(null);
 </script>
 
 <style>
@@ -101,22 +144,17 @@
     }
 </style>
 
-<div transition:fade={{ duration: 150 }} class="modal-wrapper grey-zone" aria-hidden="true" onclick={() => { if (!registrationPromise) { close() } }}>
+<div transition:fade={{ duration: 150 }} class="modal-wrapper grey-zone" aria-hidden="true" onclick={tryClose} onkeydown={tryClose}>
     <dialog open onclick={e => e.stopPropagation()}>
-        {#if registrationPromise && !registrationErrors}
-            <header class="text-center">
-                Scan This!
-            </header>
+        {#if registrationPromise}
             {#await registrationPromise}
+                <header class="text-center">
+                    Register
+                </header>
                 <article>
                     <div class="loader-wrapper">
                         <Spinner/>
                     </div>
-                    <small>
-                        <p>
-                            If you do not register your account with a two-factor authentication app, you will not be able to sign in.
-                        </p>
-                    </small>
                 </article>
                 <footer>
                     <button class="btn btn-outline btn-dark btn-center w-full" disabled>
@@ -124,19 +162,50 @@
                     </button>
                 </footer>
             {:then qrCode}
-                <article>
-                    <img src={qrCode} alt="QR Code">
-                    <small>
-                        <p>
-                            If you do not register your account with a two-factor authentication app. You will not be able to sign in.
-                        </p>
-                    </small>
-                </article>
-                <footer>
-                    <button class="btn btn-outline btn-dark btn-center w-full" onclick={close}>
-                        Got it!
-                    </button>
-                </footer>
+                <header class="text-center">
+                    Scan This!
+                </header>
+                <form onsubmit={e => { e.preventDefault(); loginPromise = performLogin().then(() => { loginPromise = null }); }}>
+                    <article>
+                        <img src={qrCode} alt="QR Code">
+                        <small>
+                            <p>
+                                If you do not register your account with a two-factor authentication app. You will not be able to sign in.
+                            </p>
+                        </small>
+                        <div>
+                            <label class="inline" for="two-factor">2FA Pin</label>
+                            <input
+                                bind:value={twoFactor} type="text" name="two-factor" id="two-factor"
+                                placeholder="••••••" autocomplete="one-time-code webauthn" class="two-factor"
+                                required minlength="6" maxlength="6" pattern="[0-9]{'{6}'}"
+                            >
+                            {#if loginErrors?.errors?.at(0)}
+                                <p class="error">{loginErrors.errors[0]}</p>
+                            {/if}
+                            {#if loginErrors?.properties?.email?.errors?.at(0)}
+                                <p class="error">{loginErrors.properties.email.errors[0]}</p>
+                            {/if}
+                            {#if loginErrors?.properties?.password?.errors?.at(0)}
+                                <p class="error">{loginErrors.properties.password.errors[0]}</p>
+                            {/if}
+                            {#if loginErrors?.properties?.twoFactor?.errors?.at(0)}
+                                <p class="error">{loginErrors.properties.twoFactor.errors[0]}</p>
+                            {/if}
+                        </div>
+                    </article>
+                    <footer>
+                        {#await loginPromise}
+                            <button class="btn btn-outline btn-dark btn-center w-full" disabled>
+                                <Spinner/>
+                            </button>
+                        {:then}
+                            <button class="btn btn-outline btn-dark btn-center w-full">
+                                Got it!
+                            </button>
+                        {/await}
+                    </footer>
+                </form>
             {/await}
         {:else}
             <header>
@@ -145,7 +214,7 @@
                     <X class="full"/>
                 </button>
             </header>
-            <form bind:this={form} onsubmit={e => { e.preventDefault(); registrationPromise = performRegisteration() }}>
+            <form onsubmit={e => { e.preventDefault(); registrationPromise = performRegisteration().then(res => { if (res) return res; else registrationPromise = null; }) }}>
                 <article>
                     {#if registrationErrors?.errors?.at(0)}
                         <p class="error">{registrationErrors.errors[0]}</p>
@@ -158,7 +227,7 @@
                             required maxlength="128"
                         >
                         {#if registrationErrors?.properties?.email?.errors?.at(0)}
-                            <p class="error">{registrationErrors.properties.email.errors.at[0]}</p>
+                            <p class="error">{registrationErrors.properties.email.errors[0]}</p>
                         {/if}
                     </div>
                     <div>
@@ -169,7 +238,7 @@
                             required maxlength="64"
                         >
                         {#if registrationErrors?.properties?.name?.errors?.at(0)}
-                            <p class="error">{registrationErrors.properties.name.errors.at[0]}</p>
+                            <p class="error">{registrationErrors.properties.name.errors[0]}</p>
                         {/if}
                     </div>
                     <div>
@@ -180,7 +249,7 @@
                             required minlength="12" maxlength="128"
                         >
                         {#if registrationErrors?.properties?.password?.errors?.at(0)}
-                            <p class="error">{registrationErrors.properties.password.errors.at[0]}</p>
+                            <p class="error">{registrationErrors.properties.password.errors[0]}</p>
                         {/if}
                     </div>
                     <div>
